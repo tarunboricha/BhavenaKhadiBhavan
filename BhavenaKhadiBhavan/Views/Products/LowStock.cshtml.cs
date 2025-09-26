3b9275c4104c989b9d4f61c8d077e47866d36a42
tarunboricha <tarunboricha2092001@gmail.com>
2025-09-26 09:16:57 +0530

    Implemented views for Products, Categories and Customers.

---
+++ BhavenaKhadiBhavan/Views/Products/LowStock.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BhavenaKhadiBhavan.Views.Products
+{
+    public class LowStockModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
